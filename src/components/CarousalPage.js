fc7f290cff605292603ab5483e89bd5ad2114a23
karthik <karthik63254@gmail.com>
2024-02-24 20:23:18 +0530

    added carousal

--- src/components/CarousalPage.js
+++ src/components/CarousalPage.js
@@ -1,32 +1,53 @@
-import React from 'react'
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import './CarousalPage.css'
 
-function CarousalPage() {
+const TextCarousel = () => {
+  const carouselStyle = {
+    backgroundImage: 'url("../assets/images/stock-3.jpg")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '50vh',
+    marginTop: '10%',
+    marginLeft: '10%',
+    padding: '50px',
+    borderRadius: '0 0 15px 15px',
+    color: 'white', // Set text color to white
+  };
+
+  const titleStyle = {
+    fontWeight: 'bold', // Make title bold
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#9300e8', // Set button color to purple
+    border: 'none',
+  };
+
   return (
-    <div className="common-background">
-      <Carousel>
-        <Carousel.Item>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
+    <Carousel>
+      <Carousel.Item>
+        <div style={carouselStyle}>
+          <h2 style={titleStyle}>Carousel 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />Nunc tincidunt, purus vel eleifend laoreet</p>
+          <button className="btn btn-primary" style={buttonStyle}>Explore More</button>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={carouselStyle}>
+          <h2 style={titleStyle}>Carousel 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />Nunc tincidunt, purus vel eleifend laoreet</p>
+          <button className="btn btn-primary" style={buttonStyle}>Explore More</button>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={carouselStyle}>
+          <h2 style={titleStyle}>Carousel 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />Nunc tincidunt, purus vel eleifend laoreet</p>
+          <button className="btn btn-primary" style={buttonStyle}>Explore More</button>
+        </div>
+      </Carousel.Item>
+    </Carousel>
   );
-}
+};
 
-export default CarousalPage;
+export default TextCarousel;
